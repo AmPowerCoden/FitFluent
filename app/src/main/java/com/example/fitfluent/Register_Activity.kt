71b0fb6f6324decf02353edc9d6bd452845ebbe9
Mario <mario.eubel@hotmail.de>
2023-01-30 14:11:00 +0100

    Fertigstellen der Funktionalität des Log ins --Es müssen jedoch noch Fehlerabfragen eingebettet werden

---
+++ app/src/main/java/com/example/fitfluent/Register_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.fitfluent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
