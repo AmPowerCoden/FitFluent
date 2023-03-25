514560211ac42554122c28ae8a05e462549de9e9
kiLL3rsp33dy <102947207+kiLL3rsp33dy@users.noreply.github.com>
2023-03-25 12:53:39 +0100

    - Upgrade BMI-Rechner - Datenbankzguriff verändert

--- app/src/main/java/com/example/fitfluent/data/DatabaseReader.kt
+++ app/src/main/java/com/example/fitfluent/data/DatabaseReader.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.fitfluent.MainActivity
 import java.time.LocalDateTime
 
 class DatabaseReader(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
@@ -137,7 +138,7 @@ class DatabaseReader(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         contentValues.put(GENDER, newuser.gender)
         contentValues.put(BMI_SCORE, newuser.bmi_score)
 
-        val sql = "UPDATE $TABLE SET $AGE = ${newuser.age}, $HEIGHT = ${newuser.height_in_cm}, $WEIGHT = ${newuser.weight_in_kg}, $CALORIE_INTAKE = ${newuser.calorie_intake}, $CALORIE_TIME = '${LocalDateTime.now().toString()}' WHERE $USERNAME = '${olduser.username}'"
+        val sql = "UPDATE $TABLE SET $AGE = ${newuser.age}, $HEIGHT = ${newuser.height_in_cm}, $WEIGHT = ${newuser.weight_in_kg}, $CALORIE_INTAKE = ${newuser.calorie_intake}, $CALORIE_TIME = '${LocalDateTime.now().toString()}', $GENDER = '${newuser.gender}', $BMI_SCORE = '${newuser.bmi_score}' WHERE $USERNAME = '${olduser.username}'"
 
         //val selection = "$USERNAME = ? AND $PASSWORD = ? AND $AGE = ? AND $HEIGHT = ? AND $WEIGHT = ? AND $CALORIE_INTAKE = ? AND $CALORIE_TIME = ?"
         //val selectionArgs = arrayOf(olduser.username, olduser.password, olduser.age.toString(), olduser.height_in_cm.toString(), olduser.weight_in_kg.toString(), olduser.calorie_intake.toString(), olduser.calorie_time)
